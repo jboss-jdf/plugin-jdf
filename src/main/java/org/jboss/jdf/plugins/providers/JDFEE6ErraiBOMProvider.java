ec215bd47ca35562e5748a2827782bb8842c18ca
Rafael Benevides <rafabene@gmail.com>
2012-06-16 17:43:59 -0300

    Revert "Changed spell of artfact in yaml file"
    This reverts commit a89649f4360877a841fc17052e743ed5d0e132b0.

---
+++ src/main/java/org/jboss/jdf/plugins/providers/JDFEE6ErraiBOMProvider.java
@@ -0,0 +1,13 @@
+package org.jboss.jdf.plugins.providers;
+
+
+public class JDFEE6ErraiBOMProvider extends BaseJDFBomProvider {
+	
+	
+
+	@Override
+	public String getArtfactId() {
+		return "jboss-javaee-6.0-with-errai";
+	}
+
+}
---
+++ src/main/java/org/jboss/jdf/plugins/providers/JDFEE6HibernateBOMProvider.java
@@ -0,0 +1,13 @@
+package org.jboss.jdf.plugins.providers;
+
+
+public class JDFEE6HibernateBOMProvider extends BaseJDFBomProvider {
+	
+	
+
+	@Override
+	public String getArtfactId() {
+		return "jboss-javaee-6.0-with-hibernate";
+	}
+
+}
